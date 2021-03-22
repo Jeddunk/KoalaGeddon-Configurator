174b5030162408613713d7f67a58217277006eb5
Yui <79342645+g-yui@users.noreply.github.com>
2021-03-22 00:51:02 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Koala-GUI',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/g-yui',
+    license='GNU GPLv3',
+    author='yui',
+    author_email='',
+    description='KG-GUI'
+)
