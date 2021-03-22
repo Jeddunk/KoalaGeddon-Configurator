0b7e220abfa722ba46c85c575eb5a7b9d52f5778
Yui <79342645+g-yui@users.noreply.github.com>
2021-03-22 00:57:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='Koala-GUI',
     version='1.0',
-    packages=[''],
+    packages=[#Point to main.py and to the logos folder#],
+    install_requires=[
+    "ttkthemes~=3.2.2",
+    "Pillow~=8.1.2"
+    ],
     url='https://github.com/g-yui',
     license='GNU GPLv3',
     author='yui',
